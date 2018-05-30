3997940c8f7310b11149c69ea3945b9b280c1a09
markarmando <39779451+markarmando@users.noreply.github.com>
2018-05-30 23:02:45 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Botons del index
+// boto aps
+function botoaps() {
+    var x = document.getElementById('cont1');
+    if (x.style.opacity === '0') {
+        x.style.opacity = '1';
+    } else {
+        x.style.opacity = '0';
+    }
+}
+
+// boto internet
+function botoint() {
+    var x = document.getElementById('cont2');
+    if (x.style.opacity === '0') {
+        x.style.opacity = '1';
+    } else {
+        x.style.opacity = '0';
+    }
+}
+
+// boto games
+function botogame() {
+    var x = document.getElementById('cont3');
+    if (x.style.opacity === '0') {
+        x.style.opacity = '1';
+    } else {
+        x.style.opacity = '0';
+    }
+}
